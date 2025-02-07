b10bc51cf9a4e6959f9abfec1d65a0df966392ff
zipCoder933 <sam.p.w524@gmail.com>
2025-02-07 13:37:21 -0700

    added entity collision fix
    https://github.com/CorgiTaco-Archive/No-Client-Side-Entity-Collision-Checks/blob/1.18.X/Common/src/main/resources/entitycollisionfpsfix.mixins.json

--- src/main/java/org/zipcoder/performanceTweaks/PerfTweaks.java
+++ src/main/java/org/zipcoder/performanceTweaks/PerfTweaks.java
@@ -20,23 +20,16 @@ import org.slf4j.Logger;
 // The value here should match an entry in the META-INF/mods.toml file
 @Mod(org.zipcoder.performanceTweaks.PerfTweaks.MODID)
 public class PerfTweaks {
-
-    // Define mod id in a common place for everything to reference
     public static final String MODID = "perf_tweaks";
-    // Directly reference a slf4j logger
-    //The logger is a central point for logging
     private static final Logger LOGGER = LogUtils.getLogger();
 
     public PerfTweaks() {
-        IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
+//        IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
+//        modEventBus.addListener(this::commonSetup);
 
-        // Register the commonSetup method for modloading
-        modEventBus.addListener(this::commonSetup);
-
-        // Register ourselves for server and other game events we are interested in
         MinecraftForge.EVENT_BUS.register(this);
 
-        // Register our mod's ForgeConfigSpec so that Forge can create and load the config file for us
+//Register the config
 //        ModLoadingContext.get().registerConfig(ModConfig.Type.COMMON, Config.SPEC);
 
         /**
@@ -46,25 +39,19 @@ public class PerfTweaks {
                 IExtensionPoint.DisplayTest.class,
                 () -> new IExtensionPoint.DisplayTest(() -> NetworkConstants.IGNORESERVERONLY, (a, b) -> true)
         );
-
     }
 
-    private void commonSetup(final FMLCommonSetupEvent event) {
+//    private void commonSetup(final FMLCommonSetupEvent event) {}
 
-    }
-
-    // You can use SubscribeEvent and let the Event Bus discover methods to call
-    @SubscribeEvent
-    public void onServerStarting(ServerStartingEvent event) {
-
-    }
-
-    // You can use EventBusSubscriber to automatically register all static methods in the class annotated with @SubscribeEvent
-    @Mod.EventBusSubscriber(modid = MODID, bus = Mod.EventBusSubscriber.Bus.MOD, value = Dist.CLIENT)
-    public static class ClientModEvents {
-        @SubscribeEvent
-        public static void onClientSetup(FMLClientSetupEvent event) {
-
-        }
-    }
+//    @SubscribeEvent
+//    public void onServerStarting(ServerStartingEvent event) {}
+//
+//    // You can use EventBusSubscriber to automatically register all static methods in the class annotated with @SubscribeEvent
+//    @Mod.EventBusSubscriber(modid = MODID, bus = Mod.EventBusSubscriber.Bus.MOD, value = Dist.CLIENT)
+//    public static class ClientModEvents {
+//        @SubscribeEvent
+//        public static void onClientSetup(FMLClientSetupEvent event) {
+//
+//        }
+//    }
 }
